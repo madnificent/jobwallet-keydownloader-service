1f37f7fe8cb2a582e7bfb1fe6157cd6225995147
Aad Versteden <madnificent@gmail.com>
2017-04-13 13:53:44 +0200

    Semi-solid fetching of URLs using redirects
    This approach isn't solid gold, but it will get us started.  The logic
    comes from stack overflow, so it must be great quality.

--- web.rb
+++ web.rb
@@ -1,5 +1,18 @@
 # see https://github.com/mu-semtech/mu-ruby-template for more info
 get '/' do
+require 'open-uri'
   content_type 'application/json'
   { data: { attributes: { hello: 'world' } } }.to_json
+def fetch_url( url )
+  # See http://stackoverflow.com/questions/27407938/ruby-open-uri-redirect-forbidden#27411667
+  uri = URI.parse(url)
+  tries = 3
+
+  begin
+    uri.open(redirect: false)
+  rescue OpenURI::HTTPRedirect => redirect
+    uri = redirect.uri # assigned from the "Location" response header
+    retry if (tries -= 1) > 0
+    raise
+  end
 end
