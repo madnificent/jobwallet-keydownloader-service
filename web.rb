f08f29f1f8417b14adb90efe760e71504ff35347
Aad Versteden <madnificent@gmail.com>
2017-04-14 11:49:17 +0200

    Download and store the jobwallets and keys
    We now download the related jobWallets and its keys.  These files are
    shared on the fileshare://location uri, which should become understood
    by the consuming microservices.

--- web.rb
+++ web.rb
@@ -5,14 +5,21 @@ get '/fetch' do
 
   location = params['location']
   doc = Nokogiri::HTML( fetch_url params['location'] )
-  wallets = fetch_rels(doc, "jobwallet")
-  keys = fetch_rels(doc, "pubkey")
+
+  wallets = fetch_rels(doc, "jobwallet").map do |wallet_location|
+    normalized_uri = URI::join location, wallet_location
+    store_remote_file normalized_uri, "wallet"
+  end
+  keys = fetch_rels(doc, "pubkey").map do |pubkey_location|
+    normalized_uri = URI::join location, pubkey_location
+    store_remote_file normalized_uri, "pubkey"
+  end
 
   {
     data: {
       attributes: {
-        wallets: wallets.map { |w| URI::join location, w },
-        keys: keys.map { |k| URI::join location, k }
+        wallets: wallets,
+        keys: keys
       }
     }
   }.to_json
@@ -30,7 +37,7 @@ end
 ## Helper for fetching a URL
 def fetch_url( url )
   # See http://stackoverflow.com/questions/27407938/ruby-open-uri-redirect-forbidden#27411667
-  uri = URI.parse(url)
+  uri = url.class == String ? URI.parse(url) : url
   tries = 3
 
   begin
@@ -40,4 +47,21 @@ def fetch_url( url )
     retry if (tries -= 1) > 0
     raise
   end
+end
+
+def write_to_file( content, base="wallet-data" )
+  base ||= "wallet-data"
+  rel_filename = "#{base}-#{generate_uuid}"
+  full_path = "/fileshare/wallets/#{rel_filename}"
+
+  File.write( full_path, content )
+
+  "fileshare://#{rel_filename}"
+end
+
+def store_remote_file( uri, base_filename )
+  content = fetch_url uri
+  pathname = write_to_file( content, base_filename )
+
+  { uri: uri, pathname: pathname }
 end
