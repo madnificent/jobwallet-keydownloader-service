3c8416361ca908b35c38c03f5ef6895e0657d44f
Aad Versteden <madnificent@gmail.com>
2017-04-14 22:58:39 +0200

    Automatically fetch authorities in triplestore
    If an authority is added to the triplestore, then we automatically
    fetch the wallet and populate the item.  Thereby allowing for
    automatic downloading of related jobwallets.

--- web.rb
+++ web.rb
@@ -1,5 +1,38 @@
 require 'open-uri'
 
+post "/delta" do
+  request.body.rewind
+  payload = JSON.parse request.body.read
+
+  created_entities = []
+
+  payload["delta"].each do |delta|
+    if delta["graph"] == "http://mu.semte.ch/application"
+      delta["inserts"].map do |insert|
+        is_type_declaration = insert["p"]["value"] == "http://www.w3.org/1999/02/22-rdf-syntax-ns#type"
+        is_authorative_body = insert["o"]["value"] == "http://mu.semte.ch/vocabularies/ext/AuthorativeBody"
+        if is_type_declaration and is_authorative_body
+          created_entities << insert["s"]["value"]
+        end
+      end
+    end
+  end
+
+  found_entities = created_entities.map do |authorative_body_uri|
+    unless authorative_body_is_fetched? authorative_body_uri
+      log.info "Fetching authorative body #{authorative_body_uri}"
+      fetch_authorative_body_info authorative_body_uri, false
+    end
+  end
+
+  if found_entities.length > 0
+    log.info "Found #{found_entities.size} authorative bodies"
+    log.info found_entities.to_json
+  end
+
+  return 204
+end
+
 get %r{fetch/?} do
   content_type 'application/json'
 
@@ -10,9 +43,23 @@ get %r{fetch/?} do
   { data: { attributes: creation_info } }.to_json
 end
 
+## Returns truethy if the authorative body was fetched before
+def authorative_body_is_fetched?( authorative_body )
+  query <<SPARQL
+    PREFIX ext: <http://mu.semte.ch/vocabularies/ext/>
+    PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
+
+    ASK {
+      GRAPH <#{settings.graph}> {
+        <#{authorative_body}> ext:isFetched "true".
+      }
+    }
+SPARQL
+end
+
 ## Downloads the authorative body to fetch the necessary contents
-def fetch_authorative_body_info( location )
-  doc = Nokogiri::HTML( fetch_url params['location'] )
+def fetch_authorative_body_info( location, generate = true )
+  doc = Nokogiri::HTML( fetch_url location )
 
   wallets = fetch_rels(doc, "jobwallet").map do |wallet_location|
     normalized_uri = URI::join location, wallet_location
@@ -27,9 +74,12 @@ def fetch_authorative_body_info( location )
     <#{location}> a ext:AuthorativeBody;
       ext:isFetched "true";
       ext:hasWallet #{wallets.map { |w| "<#{w[:uri]}>" }.join(",") };
-      ext:hasPubkey #{keys.map { |k| "<#{k[:uri]}>" }.join(",") };
-      mu:uuid "#{generate_uuid}".
+      ext:hasPubkey #{keys.map { |k| "<#{k[:uri]}>" }.join(",") }.
 TTL
+  authorative_uuid = <<TTL
+    <#{location} mu:uuid "#{generate_uuid}".
+TTL
+
   wallets_data = wallets.map do |wallet|
     <<TTL
       <#{wallet[:uri]}> a ext:JobWallet;
@@ -52,6 +102,7 @@ TTL
     INSERT DATA {
       GRAPH <#{settings.graph}> {
  #{authorative_body_data}
+ #{authorative_uuid if generate}
  #{wallets_data}
  #{keys_data}
       }
