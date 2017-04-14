7a408ee9b0d1abaea2d09b1d9c6f9716ecd52797
Aad Versteden <madnificent@gmail.com>
2017-04-14 10:10:51 +0200

    Helper for fetching a relation from a document
    This helper makes it easier to fetch the href relations mentioned in a
    document.  We intend to use this method of communication for
    identifying the ownership of a jobwallet.

--- web.rb
+++ web.rb
@@ -3,6 +3,12 @@ get '/' do
 require 'open-uri'
   content_type 'application/json'
   { data: { attributes: { hello: 'world' } } }.to_json
+def fetch_rels( nokogiri_document, relation, attr="href" )
+  nokogiri_document.css("link[rel=#{relation}]").map do |element|
+    element.attribute(attr).value
+  end
+end
+
 def fetch_url( url )
   # See http://stackoverflow.com/questions/27407938/ruby-open-uri-redirect-forbidden#27411667
   uri = URI.parse(url)
